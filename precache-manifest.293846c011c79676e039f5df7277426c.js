12e6c3339baf452003f4850b5f120c8e640ae172
MaximilianLau <43243180+MaximilianLau@users.noreply.github.com>
2020-06-03 10:34:39 -0400

    Updates

---
+++ precache-manifest.293846c011c79676e039f5df7277426c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb48f74ad67698db6dea468546fad12",
+    "url": "/iDemograph/index.html"
+  },
+  {
+    "revision": "3cdb9dbcda974af5b855",
+    "url": "/iDemograph/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c6d26d1b25c407f74550",
+    "url": "/iDemograph/static/css/main.5321bd76.chunk.css"
+  },
+  {
+    "revision": "3cdb9dbcda974af5b855",
+    "url": "/iDemograph/static/js/2.458e5735.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/iDemograph/static/js/2.458e5735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d26d1b25c407f74550",
+    "url": "/iDemograph/static/js/main.2aa9f3f5.chunk.js"
+  },
+  {
+    "revision": "9e8ba2b7be96991d4048",
+    "url": "/iDemograph/static/js/runtime-main.ef7c912c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MaximilianLau/iDemograph/precache-manifest.6b7b05e8a22dcf1ae9b68e0a5c20927f.js"
+  "/iDemograph/precache-manifest.293846c011c79676e039f5df7277426c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MaximilianLau/iDemograph/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iDemograph/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
